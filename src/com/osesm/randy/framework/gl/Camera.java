b49e74e57b85a2771d1a04e6f293f1d7e1765f09
bryanl <bryan@osesm.com>
2012-02-05 13:06:08 -0500

    Use Vector3s to describe the look at matrix

--- src/com/osesm/randy/framework/gl/Camera.java
+++ src/com/osesm/randy/framework/gl/Camera.java
@@ -3,6 +3,7 @@ package com.osesm.randy.framework.gl;
 import android.opengl.Matrix;
 
 import com.osesm.randy.framework.math.Matrix4;
+import com.osesm.randy.framework.math.Vector3;
 
 public class Camera {
 
@@ -11,13 +12,44 @@ public class Camera {
 	private Matrix4 viewMatrix = new Matrix4();
 	private Matrix4 projectionMatrix = new Matrix4();
 
+	private Vector3 eye;
+	private Vector3 up;
+	private Vector3 center;
+
 	public Camera(int width, int height) {
 		this.width = width;
 		this.height = height;
 
-		Matrix.setLookAtM(viewMatrix.values(), 0, 0f, -2.5f, -4f, 0f, 0f, 0f, 0f, 1.0f,
-				0.0f);
+		eye = new Vector3(0f, -2.5f, -4f);
+		center = new Vector3(0, 0, 0);
+		up = new Vector3(0, 1f, 0);
 
+		Matrix.setLookAtM(viewMatrix.values(), 0, eye.x, eye.y, eye.z, center.x,
+				center.y, center.z, up.x, up.y, up.z);
+	}
+
+	public Vector3 getEye() {
+		return eye;
+	}
+
+	public void setEye(Vector3 eye) {
+		this.eye = eye;
+	}
+
+	public Vector3 getUp() {
+		return up;
+	}
+
+	public void setUp(Vector3 up) {
+		this.up = up;
+	}
+
+	public Vector3 getCenter() {
+		return center;
+	}
+
+	public void setCenter(Vector3 center) {
+		this.center = center;
 	}
 
 	public Matrix4 getViewProjectionMatrix() {
@@ -30,7 +62,7 @@ public class Camera {
 	}
 
 	private Matrix4 getProjection() {
-		float ratio = (float) width / height;
+//		float ratio = (float) width / height;
 
 		// TODO This is where we want to be
 		float h = 4.0f * height / width;
